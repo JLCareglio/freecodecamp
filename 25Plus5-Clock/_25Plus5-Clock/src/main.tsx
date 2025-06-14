d426c91e7f91831fd049a1b82763775e44440e23
Jesús Lautaro Careglio Albornoz <jlcareglioalbornoz@gmail.com>
2025-06-14 08:36:22 +0000

    🩹 Fix Auto-Test-Selector

--- 25Plus5-Clock/_25Plus5-Clock/src/main.tsx
+++ 25Plus5-Clock/_25Plus5-Clock/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import initializeTestSuiteObserver from './utils/testSuiteObserver';
 import './index.css';
+
+initializeTestSuiteObserver();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ 25Plus5-Clock/_25Plus5-Clock/src/utils/testSuiteObserver.ts
@@ -0,0 +1,21 @@
+function initializeTestSuiteObserver(): void {
+  const observer = new MutationObserver((_mutations: MutationRecord[], obs: MutationObserver) => {
+    const testSuiteSelector = document
+      .querySelector('#fcc_test_suite_wrapper')
+      ?.shadowRoot?.querySelector('#test-suite-selector') as HTMLSelectElement | null;
+
+    if (testSuiteSelector) {
+      testSuiteSelector.value = '25-5-clock';
+      const changeEvent = new Event('change', { bubbles: true });
+      testSuiteSelector.dispatchEvent(changeEvent);
+      obs.disconnect();
+    }
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
+}
+
+export default initializeTestSuiteObserver;
